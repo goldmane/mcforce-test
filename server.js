ecd12e45a81805e9d2abe0960234e831ec101742
Eric Goldman <egoldman@salesforce.com>
2015-08-05 18:34:17 -0400

    Fix port

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use('/api', router);
 app.use(express.static(__dirname + '/static'));
 
-var server = app.listen(5000, function () {
+var server = app.listen(process.env.PORT || 5000, function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('App listening at:', server.address());
