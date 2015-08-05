3647b27571876568a649fe6eb8c2c7b8f887ff3c
Eric Goldman <egoldman@salesforce.com>
2015-08-05 17:31:48 -0400

    Initial checkin

---
+++ server.js
@@ -0,0 +1,19 @@
+// JavaScript source code
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var request = require('request');
+var router = express.Router();
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+app.use('/api', router);
+app.use(express.static(__dirname + '/static'));
+
+var server = app.listen(5000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('App listening at:', server.address());
+});
